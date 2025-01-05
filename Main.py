f413bd44ffa13cd6ee008d9973f564fd37c0c411
aby <abysamassa@yahoo.fr>
2025-01-05 21:28:47 +0100

    ajout de la fonction compute_number_neighbor qui doit compter le nombre de voisins vivant autour d'une cellule

--- Main.py
+++ Main.py
@@ -1,9 +1,38 @@
-import numpy
-frame = numpy.array([
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 1, 1, 0],
-    [0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0]
+import numpy 
+frame=numpy.array([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
 ])
+
+paded_frame = numpy.pad (frame, 1, mode='constant') # le code pour les bordures donnée par le prof
+
+print(paded_frame)
+
+
+def compute_number_neighbors(paded_frame, index_line, index_column,):
+    new_paded_frame=paded_frame.copy() 
+    
+    neighbors= [
+     [0, -1],
+     [1, -1],
+     [1, 0],
+     [1, 1],
+     [0, 1],
+     [-1, 1],
+     [-1, 0],
+    [-1, -1],   
+] 
+    box_neighbor= 0
+    for neighbor in neighbors:
+        neighbors_index_line = index_line + neighbor[0]
+        neighbors_index_column = index_column + neighbor[1]
+        if paded_frame[neighbors_index_line, neighbors_index_column] == 1:
+            box_neighbor+= 1
+
+    return box_neighbor
+
