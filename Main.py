25bfe9210323bbe8a1c89679dcdced644a9b3971
aby <abysamassa@yahoo.fr>
2025-01-07 21:51:43 +0100

    correction des régles du jeu

--- Main.py
+++ Main.py
@@ -34,11 +34,29 @@ def compute_number_neighbors(paded_frame, index_line, index_column,):
 
     return box_neighbor
 
-compute_number_neighbors()
-
 def compute_next_frame(paded_frame, index_line, index_column):
+    paded_frame
+    new_paded_frame= numpy.array([
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+])
     
     
     for index_line in range (1, paded_frame -1):
         for index_column in range (1, paded_frame -1):
          total_of_neighbors=compute_number_neighbors(paded_frame, index_line, index_column)
+         
+         if paded_frame [index_line,index_column]==1
+         if total_of_neighbors < 2 or total_of_neighbors > 3:
+          new_paded_frame [index_line,index_column]=0
+    else:
+        if  total_of_neighbors ==3 :
+         new_paded_frame [index_column,index_line]=1
+        return new_paded_frame
+       
+       
